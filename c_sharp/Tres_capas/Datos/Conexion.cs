f94f177fca189e146a26f0ee563b83f527915a83
felixacdc <pfmata01@gmail.com>
2017-02-07 22:16:24 -0600

    [add] class connection

--- c_sharp/Tres_capas/Datos/Conexion.cs
+++ c_sharp/Tres_capas/Datos/Conexion.cs
@@ -8,6 +8,29 @@ namespace Datos
 {
     class Conexion
     {
-        
+        private SqlConnection con { get;set; }
+        private string cadenaConexion()
+        {
+            return @"Data Source=localhost;Initial Catalog=TutorialCShar; User Id=Ing; Password=mypass"
+        }
+        //metodo obtener conexion
+        public SqlConnection getConexion()
+        {
+            try {
+                con = new SqlConnection(cadenaConexion());
+                this.con.Open();
+                return this.con;
+            } catch(Exeption) {
+                return null
+            }
+        }
+        // metodo cerrar conexion
+        public void CerrarConexion()
+        {
+            if(this.con!=null)
+            {
+                this.con.Close();
+            }
+        }
     }
 }
